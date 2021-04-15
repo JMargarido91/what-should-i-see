27b0dd0ae2d2c1da63d037e1fc56ccfa1ef251de
João Margarido <joaopedromargarido@gmail.com>
2021-04-15 16:27:52 +0100

    added imdb information and genre in moviedetails

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = ({searchText, setSearchText}) => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            Movie Browser
+            WSIS
           </Link>
           <button
             className="navbar-toggler"
